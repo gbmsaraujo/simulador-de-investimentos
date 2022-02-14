cea3ae00d35ced255e8b5f43d097fdadc0d3cb49
gbmsaraujo <gbmsaraujo@gmail.com>
2022-02-14 15:02:43 -0300

    Ajuste no Css e HTML para otimização do JS e diminuição de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,20 @@
-
 /* ----------------------------------Fazendo os Imports--------------------------------- */
-const bruto = document.querySelector(".bruto");
-const liquido = document.querySelector(".liquido");
-const pre = document.querySelector(".pre")
-const pos = document.querySelector(".pos")
-const fixado = document.querySelector(".fixado")
-const tagImg = document.querySelectorAll(".tag-img");
-const aporteRent = document.getElementById('aporte-r')
-const prazo = document.getElementById('prazo-r')
-const ipca = document.getElementById('ipca')
-const aporteIndex = document.getElementById('aporte-i')
-const rentabilidade = document.getElementById('rentabilidade')
-const cdiRent = document.getElementById('cdi')
-const simular = document.getElementById('simular')
-const limpar =document.getElementById ('limpar')
-const labelClass = document.querySelectorAll('.labelClass')
-const formGroup = document.querySelectorAll('.form-group')
-
+const bruto = document.getElementById("bruto");
+const liquido = document.getElementById("liquido");
+const pre = document.getElementById("pre");
+const pos = document.getElementById("pos");
+const fixado = document.getElementById("fixado");
+const aporteRent = document.getElementById("aporte-r");
+const prazo = document.getElementById("prazo-r");
+const ipca = document.getElementById("ipca");
+const aporteIndex = document.getElementById("aporte-i");
+const rentabilidade = document.getElementById("rentabilidade");
+const cdiRent = document.getElementById("cdi");
+const simular = document.getElementById("simular");
+const limpar = document.getElementById("limpar");
+const labelClass = document.querySelectorAll(".labelClass");
+const formGroup = document.querySelectorAll(".form-group");
+const msgP = document.querySelectorAll('.msg-p')
 
 /*----------------- Codificando a Parte de Rendimento ----------------------*/
 
@@ -24,128 +22,122 @@ let ativo = false;
 
 bruto.addEventListener("click", (event) => {
   ativo = true;
-  tagImg.forEach((element, i) => {
-    if (i === 0) {
-      element.setAttribute("src", "./assets/img/icons/check-svgrepo-com 1.svg");
-    }
-  });
-  bruto.style.background = "#ED8E53";
-  bruto.style.color = "white";
-  liquido.style.background = "#EFEFEF";
-  liquido.style.color = "black";
-  tagImg[1].setAttribute("src", "");
+
+  bruto.removeAttribute("button-disabled");
+  bruto.setAttribute("class", "button-enabled");
+
+  liquido.removeAttribute("button-enabled");
+  liquido.setAttribute("class", "button-disabled");
 });
 
 liquido.addEventListener("click", (event) => {
   if (ativo == true) {
-    bruto.style.background = "#EFEFEF";
-    bruto.style.color = "black";
-    tagImg[0].setAttribute("src", "");
+    bruto.removeAttribute("button-enabled");
+    bruto.setAttribute("class", "button-disabled");
   }
 
-  tagImg.forEach((element, i) => {
-    if (i === 1) {
-      element.setAttribute("src", "./assets/img/icons/check-svgrepo-com 1.svg");
-    }
-  });
-
-  liquido.style.background = "#ED8E53";
-  liquido.style.color = "white";
-
+  liquido.removeAttribute("button-disabled");
+  liquido.setAttribute("class", "button-enabled");
 });
 
-aporteRent.addEventListener('focus',()=>{
-    aporteRent.removeAttribute('placeholder')
-})
+aporteRent.addEventListener("focus", () => {
+  aporteRent.removeAttribute("placeholder");
+  aporteRent.value = "R$ ";
+});
 
 /*----------------- Codificando a Parte de Indexação ----------------------*/
 
 pre.addEventListener("click", (event) => {
-    tagImg.forEach((element, i) => {
-      if (i === 2) {
-        element.setAttribute("src", "./assets/img/icons/check-svgrepo-com 1.svg");
-      }
-    });
-    pre.style.background = "#ED8E53";
-    pre.style.color = "white";
-    pos.style.background = "#EFEFEF";
-    pos.style.color = "black";
-    fixado.style.background = "#EFEFEF";
-    fixado.style.color = "black";
-    tagImg[3].setAttribute("src", "");
-    tagImg[4].setAttribute("src", "");
-  });
+
+  pre.removeAttribute("button-disabled");
+  pre.setAttribute("class", "button-enabled");
+
+  pos.removeAttribute("button-enabled");
+  pos.setAttribute("class", "button-disabled");
+
+  fixado.removeAttribute("button-enabled");
+  fixado.setAttribute("class", "button-disabled");
+});
 
 pos.addEventListener("click", (event) => {
-    tagImg.forEach((element, i) => {
-      if (i === 3) {
-        element.setAttribute("src", "./assets/img/icons/check-svgrepo-com 1.svg");
-      }
-    });
-    pos.style.background = "#ED8E53";
-    pos.style.color = "white";
-    pre.style.background = "#EFEFEF";
-    pre.style.color = "black";
-    fixado.style.background = "#EFEFEF";
-    fixado.style.color = "black";
-    tagImg[2].setAttribute("src", "");
-    tagImg[4].setAttribute("src", "");
-  });
+  pos.removeAttribute("button-disabled");
+  pos.setAttribute("class", "button-enabled");
+
+  pre.removeAttribute("button-enabled");
+  pre.setAttribute("class", "button-disabled");
+
+  fixado.removeAttribute("button-enabled");
+  fixado.setAttribute("class", "button-disabled");
+});
 
 fixado.addEventListener("click", (event) => {
-    tagImg.forEach((element, i) => {
-      if (i === 4) {
-        element.setAttribute("src", "./assets/img/icons/check-svgrepo-com 1.svg");
-      }
-    });
-    fixado.style.background = "#ED8E53";
-    fixado.style.color = "white";
-    pre.style.background = "#EFEFEF";
-    pre.style.color = "black";
-    pos.style.background = "#EFEFEF";
-    pos.style.color = "black";
-    tagImg[2].setAttribute("src", "");
-    tagImg[3].setAttribute("src", "");
-  });
+  fixado.removeAttribute("button-disabled");
+  fixado.setAttribute("class", "button-enabled");
 
-  aporteIndex.addEventListener('focus',()=>{
-    aporteIndex.removeAttribute('placeholder')
-})
-  
- aporteIndex.addEventListener('focus',()=>{
-    aporteIndex.removeAttribute('placeholder')
-})
+  pre.removeAttribute("button-enabled");
+  pre.setAttribute("class", "button-disabled");
+
+  pos.removeAttribute("button-enabled");
+  pos.setAttribute("class", "button-disabled");
+
+});
+
+aporteIndex.addEventListener("focus", () => {
+  aporteIndex.removeAttribute("placeholder");
+  aporteIndex.value = "R$ ";
+});
 
 /*-------------------------------- Validando os Dados-------------------------------*/
-const creatP = document.createElement('p')
 
-  function msgErro (msg,i) {
-      creatP.innerText = msg
-      creatP.style.color = 'red'
-      formGroup[i].appendChild(creatP)
+simular.addEventListener("click", () => {
+ 
+  let aporteValueRent = Number(aporteRent.value.slice(2).trim().replace(",", "."));
+  let aporteValueIndex = Number(aporteIndex.value.slice(2).trim().replace(",", "."));
+  let prazoValue = Number(prazo.value);
+  let rentabilidadeValue = Number(rentabilidade.value.slice(0,2))
+
+  if (aporteValueRent === 0 || aporteValueRent === NaN) {
+    labelClass[0].style.color = "red";
+    msgP[0].setAttribute('class', 'msg-erro')
+  } else {
+      labelClass[0].style.color = "black";
+      msgP[0].setAttribute('class', 'msg-p')
+  }
+  
+  console.log (prazoValue)
+
+  if (isNaN(prazoValue) || prazoValue <=0 || prazoValue>12){
+    labelClass[1].style.color = "red";
+    msgP[1].setAttribute('class', 'msg-erro')
+  } else {
+    labelClass[1].style.color = "black";
+    msgP[1].setAttribute('class', 'msg-p')
+  }
+
+  if (aporteValueIndex === 0 || aporteValueIndex === NaN) {
+    labelClass[3].style.color = "red";
+    msgP[3].setAttribute('class', 'msg-erro')
+  } else {
+      labelClass[3].style.color = "black";
+      msgP[3].setAttribute('class', 'msg-p')
+  }
+  
+  if (isNaN(rentabilidadeValue) || rentabilidadeValue <=0){
+    labelClass[4].style.color = "red";
+    msgP[4].setAttribute('class', 'msg-erro')
+  } else {
+    labelClass[4].style.color = "black";
+    msgP[4].setAttribute('class', 'msg-p')
   }
 
 
-
-simular.addEventListener('click', ()=>{
-    let aporteValueRent = Number(aporteRent.value.replace(',','.'))
-    let prazoValue = Number(prazo.value)
-
-    if (isNaN(aporteValueRent)){
-        msgErro ('Aporte deve ser um número válido', 0)
-        labelClass[0].style.color = 'red' 
-    } else {
-        labelClass[0].style.color = 'black'
-        creatP.innerText = '' 
-    }
-    
-    
-    if (isNaN(prazoValue)){
-        msgErro ('Prazo deve ser um número válido', 1)
-        labelClass[1].style.color = 'red' 
-    } else {
-        labelClass[1].style.color = 'black'
-        
-    }
-
 })
+ 
+
+
+
+
+
+
+
+
